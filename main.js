be0a83e1796e0ed2b09ecab03376f3af2cf7aa8f
Sam Whang <sangwoowhang@gmail.com>
2017-09-26 11:22:18 -0500

    Completed a test file

---
+++ main.js
@@ -0,0 +1,22 @@
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
+let mainWindow
+function createWindow() {
+    mainWindow = new BrowserWindow({width: 800, height: 600, minWidth: 800, minHeight: 600})
+    mainWindow.loadURL(`file://${__dirname}/helloworld.html`)
+    mainWindow.on('closed', function() {
+        mainWindow = null
+    })
+}
+app.on('ready', createWindow)
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
+})
+app.on('activate', function () {
+    if (mainWindw === null) {
+        createWindow()
+    }
+})
